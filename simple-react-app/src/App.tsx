64cb57fa6681f2c5ad507e7bd1c710975f1fab4f
keidson299 <eidson.kyle@gmail.com>
2025-08-21 20:53:03 -0400

    Added more backend functionality and developed reusable services and hooks for the http commands and data

--- simple-react-app/src/App.tsx
+++ simple-react-app/src/App.tsx
@@ -1,26 +1,97 @@
-import axios, { AxiosError, CanceledError } from "axios";
-import { useEffect, useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import ListGroup from "./components/ListGroup";
-import Like from "./components/Like";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
-import ExpenseList from "./expenseTracker/components/ExpenseList";
-import ExpenseFilter from "./expenseTracker/components/ExpenseFilter";
-import ExpenseForm from "./expenseTracker/components/ExpenseForm";
-import categories from "./expenseTracker/categories";
-import ProductList from "./components/ProductList";
-
-interface User {
-  id: number;
-  name: string;
-}
+import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  /*   let items = [
+  const { users, error, isLoading, setUsers, setError } = useUsers();
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    userService.delete(user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Kyle" };
+    setUsers([newUser, ...users]);
+
+    userService
+      .add(newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    userService.update(updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  return (
+    <div>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+// Older code for reference
+// import Alert from "./components/Alert";
+// import Button from "./components/Button/Button";
+// import ListGroup from "./components/ListGroup";
+// import Like from "./components/Like";
+// import NavBar from "./components/NavBar";
+// import Cart from "./components/Cart";
+// import ExpandableText from "./components/ExpandableText";
+// import Form from "./components/Form";
+// import ExpenseList from "./expenseTracker/components/ExpenseList";
+// import ExpenseFilter from "./expenseTracker/components/ExpenseFilter";
+// import ExpenseForm from "./expenseTracker/components/ExpenseForm";
+// import categories from "./expenseTracker/categories";
+// import ProductList from "./components/ProductList";
+
+/*   let items = [
     "New York",
     "San Francisco",
     "Tokyo",
@@ -74,45 +145,20 @@ function App() {
 
   const [category, setCategory] = useState(""); */
 
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
+// useEffect(() => {
+//   const fetchUsers = async () => {
+//     try {
+//       const res = await axios.get<User[]>(
+//         "https://jsonplaceholder.typicode.com/users"
+//       );
+//       setUsers(res.data);
+//     } catch (err) {
+//       setError((err as AxiosError).message);
+//     }
+//   };
+//   fetchUsers();
 
-  useEffect(() => {
-    // const fetchUsers = async () => {
-    //   try {
-    //     const res = await axios.get<User[]>(
-    //       "https://jsonplaceholder.typicode.com/users"
-    //     );
-    //     setUsers(res.data);
-    //   } catch (err) {
-    //     setError((err as AxiosError).message);
-    //   }
-    // };
-    // fetchUsers();
-    const controller = new AbortController();
-
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
-        signal: controller.signal,
-      })
-      .then((res) => setUsers(res.data))
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return (
-    <div>
-      {error && <p className="text-danger">{error}</p>}
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      {/*
+/*<div>
       <select
         className="form-select"
         onChange={(event) => setCategory(event.target.value)}
@@ -152,9 +198,5 @@ function App() {
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
-      <Like onLikeClick={() => console.log("clicked")} /> */}
-    </div>
-  );
-}
-
-export default App;
+      <Like onLikeClick={() => console.log("clicked")} />
+    </div>*/
